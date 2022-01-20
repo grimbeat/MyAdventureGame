179330b02bc26e2ea7fcb5333fe79eb0c7ce0400
grimbeat <soobin131@gmail.com>
2022-01-20 01:40:08 -0500

    almost done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your Code Here
+let firstAnswer  = window.prompt('Do you head left or right?')
+if(firstAnswer === 'left'){
+    let secondAnswer = window.prompt(`You come across a stray cat. 
+It scampers off down a small hole, just large enough for you to crawl through. 
+Do you follow it, or continue on your path?`)
+if (secondAnswer === 'follow'){
+    let thirdAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
+}
+if (thirdAnswer === 'stay'){
+   let fourthAnswer = window.prompt ('You live happily amongst the cats for the rest of your days.')
+}
+
+} else if(firstAnswer === 'right') {
+    let secondAnswer = window.prompt(`You come across a snoring dragon. 
+On the other side of him, you see a shiny chest of treasure. Another path would 
+lead you away from the dragon altogether. Which path do you take?`)
+
+}
