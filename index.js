d9c265e4d0cbe9cd9471d2a92cf37b00e475fee9
grimbeat <soobin131@gmail.com>
2022-01-20 01:57:39 -0500

    almost done

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-// Your Code Here
 let firstAnswer  = window.prompt('Do you head left or right?')
 if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
+}
 if (secondAnswer === 'follow'){
     let thirdAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
 }
+else if (secondAnswer === 'continue'){
+    let thirdAnswer = window.prompt('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?')
+}
 if (thirdAnswer === 'stay'){
-   let fourthAnswer = window.prompt ('You live happily amongst the cats for the rest of your days.')
+   let notice = window.alert('You live happily amongst the cats for the rest of your days.')
+}
+else if (thridAnswer === 'spread the word'){
+    let notice = window.alert('You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?')
+}
+else if (thirdAnswer === 'ladder'){
+    let notice = window.alert('After ascending a few feet up the ladder, one of its rungs snaps, and you comedically fall through each of the rungs below. Sheepish, you return home.')
+}
+else if (thirdAnswer === 'staircase'){
+    let notice= window.alert('After ascending the staircase, you discover a shiny blue stone, which you take home and cherish forever.')
 }
 
-} else if(firstAnswer === 'right') {
+else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
 lead you away from the dragon altogether. Which path do you take?`)
