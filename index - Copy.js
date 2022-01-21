0f80e4d97117e9a3048b2649cd8884b0a45389bd
grimbeat <soobin131@gmail.com>
2022-01-21 18:29:55 -0500

    extra done!

---
+++ index - Copy.js
@@ -0,0 +1,22 @@
+let firstAnswer  = window.prompt('Do you head left or right?')
+
+switch (firstAnswer){
+    case 'left':
+        let secondAnswer = window.prompt(`You come across a stray cat. 
+        It scampers off down a small hole, just large enough for you to crawl through. 
+        Do you follow it, or continue on your path?`)
+        switch (secondAnswer){
+            case 'follow':
+                window.prompt(`You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.`)
+            break;
+            case 'continue':
+                window.prompt(`You come across a chamber that reaches upward to a shining light above. There is a long, winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?`)
+            break;
+        }
+    break;
+    case 'right':
+        window.prompt(`You come across a snoring dragon. 
+        On the other side of him, you see a shiny chest of treasure. Another path would 
+        lead you away from the dragon altogether. Which path do you take?`)
+    break;
+}
